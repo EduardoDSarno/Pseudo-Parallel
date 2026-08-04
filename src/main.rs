71b227f322426d3b3846146740eb409ce083bcc7
eduardosarno <eduartesarno@gmail.com>
2026-08-04 12:44:22 -0700

    added write to file function where we will store the addresses seen in a simple text file and we will load up a hash set in the beginnig of each restart with the seen addresses

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::{collections::HashSet, fs::{self, File}, writeln};
+use std::io::{BufWriter, Write};
 use futures::StreamExt;
-use hypersdk::hypercore::{self, Incoming, types::Subscription, ws::Event};
+use hypersdk::hypercore::{self, Incoming, Trade, types::Subscription, ws::Event};
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = simple_logger::init_with_level(log::Level::Info);
 
+    let mut writer = BufWriter::new(File::create("data/addresses.txt")?);
+
+    let mut  addresses: HashSet<String> = HashSet::new();
+
+    
     let client = hypercore::mainnet();
     let mut ws = client.websocket();
 
@@ -30,12 +37,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             {
                 for trade in trades 
                 {
-                    log::info!("Received trade: {:?}", trade);
+                    write_to_file( &mut writer, &mut addresses, trade)?;
                 }
             }
              Event::Message(_) => {} // for all rest of the messages we don't care about
         }
     }
 
+    Ok(())
+}
+
+/* This function tries writing to our file the addresses of the buyer and sellet */
+pub fn write_to_file(writer: &mut BufWriter<File>, 
+                    addresses: &mut HashSet<String>,
+                     trade: Trade ) 
+                     -> Result<(), Box<dyn std::error::Error>>
+{
+
+    log::info!("Received trade: {:?}", trade);
+
+                    let address_buyer:  String = trade.users[0].to_string();
+                    let address_seller: String = trade.users[1].to_string();
+
+                    // address.insert returns true when address is not in the set
+                    // so we don't need a separate contains
+                    // wrinteln is a macro to append to the file
+                    if addresses.insert(address_buyer.clone()) 
+                    {
+                        writeln!(writer, "{address_buyer}")?;
+                    }
+                    
+                    if addresses.insert(address_seller.clone())
+                    {
+                        writeln!(writer, "{address_seller}")?;
+                    }
     Ok(())
 }
